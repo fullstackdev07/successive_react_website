0871d5ebd12b01b8b4af1e04b12f483fc3176a4f
Pratiyush2003 <pratiyush12@outlook.in>
2024-09-26 18:48:55 +0530

    changes related to scroll anim

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -7,7 +7,7 @@ const ServiceSection = () => {
     return (
         <div className="mx-4">
             <article
-                className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto px-4 bg-mediumgreen rounded-xl shadow-lg p-6 space-y-6 md:space-y-0 md:gap-8"
+                className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto px-8 bg-mediumgreen rounded-xl shadow-lg p-6 space-y-6 md:space-y-0 md:gap-8 "
                 data-aos="fade-left"
             >
                 <div className="flex justify-center md:w-1/2">
@@ -20,7 +20,7 @@ const ServiceSection = () => {
                 </div>
 
                 <div className="md:w-1/2 ">
-                    <h1 className="text-3xl mt-[-4rem] md:text-5xl max-w-lg font-bold text-left text-gray-800 
+                    <h1 className="text-3xl md:mt-[-4rem] mt-4 md:text-5xl max-w-lg font-bold text-left text-gray-800 
                     ">
                         {sectionTitle.main}{" "}
                         {/* <span className="text-lightpink underline">
@@ -39,7 +39,7 @@ const ServiceSection = () => {
 
             <div className="flex items-center justify-center flex-col max-w-7xl mx-auto mt-10 px-4 rounded-xl p-6 space-y-8 text-center">
                 <div>
-                    <h1 className="text-3xl md:text-5xl max-w-2xl text-gray-800">
+                    <h1 className="text-3xl md:text-5xl font-bold max-w-2xl text-gray-800">
                         {callToAction.title}
                     </h1>
                 </div>
